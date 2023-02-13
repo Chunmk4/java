d1a96ac8fbde0da2595b190abb882a26e638a64d
Chunmk4 <114391043+Chunmk4@users.noreply.github.com>
2023-02-13 13:48:39 +0700

    ...

---
+++ src/bai4.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+    public class bai4 {
+        public static void main(String[] args) {
+            Date now = new Date();
+
+            System.out.println("Now is: " + now);
+        }
+    }
+
